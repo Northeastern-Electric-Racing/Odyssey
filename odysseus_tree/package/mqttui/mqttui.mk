89e16d468a7e974a1f1cd7d8e3b2d7986e638033
Jack Rubacha <rubacha.jack03@gmail.com>
2024-11-04 23:12:59 -0500

    version bumps

--- odysseus_tree/package/mqttui/mqttui.mk
+++ odysseus_tree/package/mqttui/mqttui.mk
@@ -1,5 +1,5 @@
-MQTTUI_VERSION = 0.21.0
-MQTTUI_SITE = $(call github,EdJoPaTo,mqttui,v$(MQTTUI_VERSION))
+MQTTUI_VERSION = 0.21.1
+MQTTUI_SITE = $(call github,Northeastern-Electric-Racing,mqttui,v$(MQTTUI_VERSION)-ner)
 MQTTUI_LICENSE = GPL-3.0+
 
 $(eval $(cargo-package))
